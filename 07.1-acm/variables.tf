fefd3c80b1453fef7dfb972b151c4ea94ebf3196
udaydevops-lab <darauday0@gmail.com>
2025-02-04 22:57:26 +0530

    devops practice

---
+++ 07.1-acm/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  default = "expense"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "common_tags" {
+  default = {
+    Project = "expense"
+    Environment = "dev"
+    Terraform = "true"
+    Component = "app-alb"
+  }
+}
+
+variable "zone_name" {
+  default = "prajai.online"
+}
+
+variable "zone_id" {
+  default = "Z04473913DUPDZLWO0QLR"
+}
